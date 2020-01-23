8bded586e874bcc407be9bea65a6bedfbf6e3498
Jordan Safford and Misaki Koonce <jordan@>
2020-01-23 09:04:39 -0800

    Add AddAuthor controller.

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -80,5 +80,23 @@ namespace Library.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult AddAuthor(int id)
+        {
+            var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+            ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+            return View(thisBook);
+        }
+
+        [HttpPost]
+        public ActionResult AddAuthor(Book book, int AuthorId)
+        {
+            if(AuthorId != 0)
+            {
+                _db.AuthorBooks.Add(new AuthorBook(){ AuthorId = AuthorId, BookId = book.BookId});
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
